f6023278bd98a2dbf2b8ec399fb221b1ad2f2677
Prateek Agarwal <pragar@microsoft.com>
2015-06-16 13:15:38 +0530

    asdf

--- before-install.bat
+++ before-install.bat
@@ -1,3 +1,4 @@
 REM Install Internet Information Server (IIS). 
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
+powershell.exe rm C:\inetpub\wwwroot\index.html
